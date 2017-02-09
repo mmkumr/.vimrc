3498e346f0ba43b498c8abfe50f5e80deaf7f11f
Mukesh Kumar <mmkumr.ping@gmail.com>
2017-02-09 22:34:07 +0530

    Changed code in swapescape.sh

--- swapescape.sh
+++ swapescape.sh
@@ -4,15 +4,22 @@ if [ -e ~/.vim/swp/*.swp ]
 then
         echo "Another file open in vim. Swapping cancelled!"
         sleep 3s
+        printf '\033c'
         vim $file_name
 else
         xmodmap -e "clear Lock"
         xmodmap -e "keycode 9 = Caps_Lock"
         xmodmap -e "keycode 66 = Escape"
         xmodmap -e "add Lock = Caps_Lock"
-        vim $file_name
-        xmodmap -e "clear Lock"
-        xmodmap -e "keycode 66 = Caps_Lock"
-        xmodmap -e "keycode 9 = Escape"
-        xmodmap -e "add Lock = Caps_Lock"
+	vim $file_name
+        if [ -e ~/.vim/swp/*.swp ]
+        then
+            sleep 1s
+        else
+            xmodmap -e "clear Lock"
+            xmodmap -e "keycode 66 = Caps_Lock"
+            xmodmap -e "keycode 9 = Escape"
+            xmodmap -e "add Lock = Caps_Lock"
+
+        fi
 fi
